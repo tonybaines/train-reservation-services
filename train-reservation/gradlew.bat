2d5925a6278d3a3c45a7c0b33a00797e6391c680
Tony Baines <tony.baines.1@gmail.com>
2015-01-23 14:45:10 +0000

    Fixed proxy config for the gradle wrapper scripts

--- train-reservation/gradlew.bat
+++ train-reservation/gradlew.bat
@@ -9,7 +9,7 @@
 if "%OS%"=="Windows_NT" setlocal
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS=-Dhttp.proxyHost=proxy.intra.bt.com -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy.intra.bt.com -Dhttps.proxyPort=8080
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
